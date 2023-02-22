27d2b8247b4428b133489d25f8f1bb7ebf19b4fe
ArgotePazRamiro <argote_paz_ramiro@hotmail.com>
2023-02-22 18:04:39 -0400

    basic route and controller

---
+++ app/Http/Controllers/Demo/DemoController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Demo;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DemoController extends Controller
+{
+    
+    public function Index()
+    {
+        
+        return view('about');
+
+    }// End Method
+
+    public function Contact()
+    {
+        
+        return view('contact');
+
+    }// End Method
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\Demo\DemoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::controller(DemoController::class)->group(function() {
+    Route::get('/about', 'Index');
+    Route::get('/contact', 'Contact');
+});
+
